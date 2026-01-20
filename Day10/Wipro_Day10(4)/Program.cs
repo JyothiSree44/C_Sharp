6b197d4f6bf19571a0676a8eb6cec4853209f73e
JyothiSree44 <jyothisreethanikonda44@gmail.com>
2026-01-20 18:19:48 +0530

    Today's Update

---
+++ Day10/Wipro_Day10(4)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create cryptographically secure random bytes
+        byte[] key = new byte[32]; // 256-bit key
+
+        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(key);
+        }
+
+        // Convert to Base64 string
+        string secureKey = Convert.ToBase64String(key);
+
+        Console.WriteLine("Generated New Key: " + secureKey);
+    }
+}
---
+++ Day10/Wipro_Day10(6)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Wipro_Day10_6_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer speaker = new SpeechSynthesizer();
+
+            speaker.Speak("Hello. Welcome to Wipro Day ten sound speech program.");
+
+            Console.WriteLine("Speech completed.");
+            Console.ReadLine();
+        }
+    }
+}
